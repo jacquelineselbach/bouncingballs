926cb054350233d523440ed872ee82e0bc57d7e2
jacquelinedominique <jacqueline.selbach@stud.fh-campuswien.ac.at>
2021-01-18 14:34:00 +0100

    minor changes line chart

--- src/main/java/at/ac/fhcampuswien/bouncingballs/SimulationController.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/SimulationController.java
@@ -185,7 +185,7 @@ public class SimulationController {
 
                 // line chart
                 Circle c = new Circle(1,state.getColor());
-                c.setTranslateX(timer.getInstants() / 6.5);
+                c.setTranslateX(timer.getInstants() / 8.0);
                 c.setTranslateY(populationSize - currentPopulation.get(state));
                 chart.getChildren().add(c);
 
