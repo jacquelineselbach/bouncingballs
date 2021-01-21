aa047c735c58dde6caefc2db77591378c31401e0
jacquelinedominique <jacqueline.selbach@stud.fh-campuswien.ac.at>
2021-01-21 15:56:13 +0100

    comments app class

--- src/main/java/at/ac/fhcampuswien/bouncingballs/App.java
+++ src/main/java/at/ac/fhcampuswien/bouncingballs/App.java
@@ -7,28 +7,42 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+/* javafx.application < openjdk-15 > - called when the application is starting. */
+
 public class App extends Application {
 
+    /* start method is the main entry point for all JavaFX applications - must be overridden. */
     @Override
     public void start(Stage primaryStage) {
         try {
+            // set up of the start scene
             FXMLLoader loader = new FXMLLoader();
-
-            Parent root = loader.load(getClass().getResource("/uiStart.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/uiStart.fxml"));
             primaryStage.setScene(new Scene(root));
 
-            primaryStage.setResizable(false);   // prevents window from resizing
+            // set up of the stage
+            primaryStage.setResizable(false); // prevents window from resizing
             primaryStage.setTitle("Bouncing Balls"); // sets title of the scene
-            primaryStage.getIcons().add(new Image("images/bb-logo.png"));
-            primaryStage.show();
+            primaryStage.getIcons().add(new Image("images/bb-logo.png")); // gets the logo
+            primaryStage.show(); // shows the actual window
 
+            /* Exception e indicates any kind of conditions that a reasonable application might want to catch */
         } catch (Exception e) {
+
+            /* printStackTrace() method of throwable class prints the throwable along with other details
+               like the line number and class name where the exception occurred.*/
             e.printStackTrace();
+
+            /* java.lang.System.exit() method terminates running java virtual machine and takes a status code.
+               A non-zero value of status code is generally used to indicate abnormal termination. */
             System.exit(1);
         }
     }
 
     public static void main (String[]args) {
+
+        /* launches a standalone application and is typically called from the main method().
+           It must not be called more than once or an exception will be thrown. */
         launch(args);
     }
 }
